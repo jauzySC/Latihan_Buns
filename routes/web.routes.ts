6a7ce2805902d50c3c35b046d9ac2d9f4292f3dd
jauzySC <jausyantheswag@gmail.com>
2026-02-15 20:19:39 +0700

    feat- web

--- routes/web.routes.ts
+++ routes/web.routes.ts
@@ -10,6 +10,7 @@ web.get('/schools', schoolsController.getListData)
 web.get('school/:id', schoolsController.getDetailData)
 web.get('/classes', classesController.getListData)
 web.get('/classes/:id', classesController.getDetailData)
+web.get('/classes/:id', classesController.getDetailData)
 
 
 export default web
